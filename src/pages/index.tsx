807db3eaeba2f8203c0592521da5ab467bc22ca4
Marcos Andrade <marcospandrade2@gmail.com>
2022-04-08 18:58:51 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import styles from './home.module.scss';
 
 export default function Home() {
     return (
         <>
             <Head>
-                <title>Inicio | ig.news</title>
+                <title>Home | ig.news</title>
             </Head>
-            <h1>Hello World</h1>
+
+            <main className={styles.contentContainer}>
+                <section className={styles.hero}>
+                    <span>👏 Hey Welcome</span>
+                    <h1>
+                        News about the <span>React</span> world.
+                    </h1>
+                    <p>
+                        Get access to all the publications <br />
+                        <span>for $9.90 month</span>
+                    </p>
+                </section>
+
+                <Image
+                    src="/images/avatar.svg"
+                    alt="Girl coding"
+                    width="334"
+                    height="520"
+                />
+            </main>
         </>
     );
 }
