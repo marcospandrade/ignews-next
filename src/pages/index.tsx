6b235184ea04392e76332096f498bf6f949f4ea7
Marcos Andrade <marcospandrade2@gmail.com>
2022-04-09 09:40:19 -0300

    SSG implements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import { GetServerSideProps } from 'next';
-import Head from 'next/head';
+import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Head from 'next/head';
+
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
+
 import styles from './home.module.scss';
+
+// Client Side
+// Server Side
+// Static Site Generation
 
 interface HomeProps {
     product: {
@@ -44,7 +50,7 @@ export default function Home({ product }: HomeProps) {
     );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
     const price = await stripe.prices.retrieve(
         'price_1KmQ7oEBKlbgH3WvNnQq84Dz'
     );
@@ -60,6 +66,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return {
         props: {
             product
-        }
+        },
+        revalidate: 60 * 60 * 24 // 24 hours
     };
 };
